13aabad4edae846159da151a0fb57748d9246c7f
HeejungCCT <jjungee@gmail.com>
2020-04-21 19:02:19 +0100

    Setup Express web server

---
+++ server.js
@@ -0,0 +1,29 @@
+// import modules
+const express = require("express"); // building the Rest api
+const bodyParser = require("body-parser"); // parse the request & create req.body object
+const cors = require("cors"); // express middleware to enable corse with various options
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Lotus Massage Centre application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
